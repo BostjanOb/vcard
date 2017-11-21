61378358dcfc517e4cf6d93263b907b71f1784c3
Bostjan Oblak <bostjan@muha.cc>
2017-11-21 10:03:36 +0100

    purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,33 @@
 const { mix } = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
+const glob = require('glob-all')
+const purgeCss = require('purgecss-webpack-plugin')
 
 mix.options({ processCssUrls: false })
-    .sass('src/app.scss', 'public')
-    .options({
-        postCss: [
-            tailwindcss('tailwind.js'),
-        ]
-    });
+	.sass('src/app.scss', 'public')
+	.options({
+		postCss: [
+			tailwindcss('tailwind.js'),
+		]
+	});
+
+if (mix.inProduction()) {
+	mix.webpackConfig({
+		plugins: [
+		  new purgeCss({
+			paths: glob.sync([
+			  path.join(__dirname, 'public/index.html')
+			]),
+			extractors: [
+			  {
+				extractor: class {
+				  static extract(content) {
+					return content.match(/[A-z0-9-:\/]+/g);
+				  }
+				},
+				extensions: ['html']
+			  }
+			]
+		  })
+	]});
+}
