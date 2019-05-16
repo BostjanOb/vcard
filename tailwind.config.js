27ee62f426130955ae953d5290ed9309ca90e5a7
Bostjan Oblak <bostjan@muha.cc>
2019-05-16 09:24:55 +0200

    update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+    	colors: {
+			trafika: '#d91e18',
+  			ekipa24: '#cd0100',
+  			salomon: '#f47321'
+		},
+    }
+  },
+  variants: {},
+  plugins: []
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,33 +1,14 @@
-const { mix } = require('laravel-mix');
+const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
-const glob = require('glob-all')
-const purgeCss = require('purgecss-webpack-plugin')
+require('laravel-mix-purgecss');
 
-mix.options({ processCssUrls: false })
-	.sass('src/app.scss', 'public')
-	.options({
-		postCss: [
-			tailwindcss('tailwind.js'),
-		]
-	});
-
-if (mix.inProduction()) {
-	mix.webpackConfig({
-		plugins: [
-		  new purgeCss({
-			paths: glob.sync([
-			  path.join(__dirname, 'public/index.html')
-			]),
-			extractors: [
-			  {
-				extractor: class {
-				  static extract(content) {
-					return content.match(/[A-z0-9-:\/]+/g);
-				  }
-				},
-				extensions: ['html']
-			  }
-			]
-		  })
-	]});
-}
+mix.sass('src/app.scss', 'public')
+  .options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind.config.js') ],
+  })
+  .purgeCss({
+  	paths: [
+		path.join(__dirname, 'public/index.html')
+    ],
+  });
